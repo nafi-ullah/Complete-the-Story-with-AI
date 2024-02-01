6be5c5735c4d7994f577d6240c9f9fc229c57e36
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-01 18:50:23 +0600

    gpt frontend fetch

--- server.js
+++ server.js
@@ -26,15 +26,13 @@ app.post('/completions', async (req, res)=>{
     try{
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
+        console.log("Backend..........")
         console.log(data);
         res.send(data);
     }catch(error){
         console.error(error);
     }
 })
-
-
-
 
 
 app.listen(PORT, () => console.log(`My server is running on port ${PORT}` ))
--- src/new_component/OutputText.js
+++ src/new_component/OutputText.js
@@ -1,6 +1,37 @@
 import React from 'react'
 
 const OutputText = () => {
+    var initiated = false;
+
+    const getMessages = async () =>{
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                messages: "how to use phone?",
+            })
+        }
+
+
+        try{
+            const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+            const data = await response.json()
+            console.log("Frontend..........")
+              console.log(data);
+        }catch(error){
+                console.error(error);
+        }
+
+        initiated = true;
+    }
+
+    useEffect(() => {
+        if (!initiated) getMessages();
+      }, []);
+    
+
   return (
     <div class="w-[550px] h-[750px] bg-gray-500"> 
 
